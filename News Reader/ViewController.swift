e90d30755256cc367aa1bb553cd7a234800a08ae
Habib Akbar <habibakbar360@gmail.com>
2021-04-27 16:43:13 +0700

    Initial Commit

---
+++ News Reader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  News Reader
+//
+//  Created by M Habib Ali Akbar on 27/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
